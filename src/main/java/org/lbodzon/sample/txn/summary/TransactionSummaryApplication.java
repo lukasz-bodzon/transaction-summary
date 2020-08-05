9e5ba5837c89dc49e24bedb1983be508e39fa14e
Lukasz Bodzon <lukasz_bodzon@epam.com>
2020-08-05 11:14:41 +0200

    Initial commit

---
+++ src/main/java/org/lbodzon/sample/txn/summary/TransactionSummaryApplication.java
@@ -0,0 +1,13 @@
+package org.lbodzon.sample.txn.summary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSummaryApplication.class, args);
+	}
+
+}
